31fbd447640cbaed6af582c451eea70352d0858d
kiwoook <tizmfns1218@Naver.com>
2024-10-15 21:39:59 +0900

    Feat: 서비스 인터페이스 생성
    구현 기능에 필요한 인터페이스 생성

---
+++ src/main/java/calculator/service/SeparatorService.java
@@ -0,0 +1,15 @@
+package calculator.service;
+
+import calculator.entity.Num;
+import calculator.entity.Separator;
+import java.util.List;
+import java.util.Set;
+
+public interface SeparatorService {
+
+    List<Num> SeparateNum(String input, String separatorString);
+
+    Set<Separator> getCustomSeparators(String input);
+
+    String refineString(Set<Separator> separatorSet);
+}
