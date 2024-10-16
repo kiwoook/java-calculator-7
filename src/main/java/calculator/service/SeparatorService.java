1097b62c37487e28b0407cb0c8c16dc83dabee4c
kiwoook <tizmfns1218@Naver.com>
2024-10-16 10:47:56 +0900

    Feat: SeparatorService 업데이트 및 구현
    - 서비스 메서드 형식 변경 및 구현

--- src/main/java/calculator/service/SeparatorService.java
+++ src/main/java/calculator/service/SeparatorService.java
@@ -3,13 +3,16 @@ package calculator.service;
 import calculator.entity.Num;
 import calculator.entity.Separator;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 public interface SeparatorService {
 
-    List<Num> separateNum(String input, String separatorString);
+    List<Num> separateNum(String refinedInput, String separatorString);
 
     Set<Separator> getCustomSeparators(String input);
 
-    String refineString(Set<Separator> separatorSet);
+    String refineString(String input, Queue<Integer> idxQueue);
+
+    Queue<Integer> getAllCustomSepIdx(String input);
 }
